d6d2d291504e861b5786067595550fba9899f540
oscar menéndez <omenendez7@gmail.com>
2020-12-14 23:00:19 -0600

    Change theme

--- next.config.js
+++ next.config.js
@@ -1,2 +1,2 @@
-const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
+const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
--- theme.config.js
+++ theme.config.js
@@ -1,35 +1,14 @@
+const YEAR = (new Date).getFullYear()
+
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
-  logo: (
-    <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
-      <span className="text-gray-600 font-normal hidden md:inline">
-        The Next Site Builder
-      </span>
-    </>
-  ),
-  head: (
-    <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the next site builder" />
-      <meta name="og:description" content="Nextra: the next site builder" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
-    </>
-  ),
-  search: true,
-  prevLinks: true,
-  nextLinks: true,
-  footer: true,
-  footerEditOnGitHubLink: true,
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>
+  footer: <small style={{ display: 'block', marginTop: '8rem' }}>
+    <abbr title='This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.' style={{cursor: 'help'}}>CC BY-NC 4.0</abbr> <time>{YEAR}</time> © Shu Ding.
+    <style dangerouslySetInnerHTML={{ __html: `
+      @media screen and (max-width: 480px) {
+        article { 
+          padding-top: 2rem;
+          padding-bottom: 4rem;
+        }
+      }` }}/>
+  </small>
 }
