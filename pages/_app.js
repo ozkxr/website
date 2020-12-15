c8a8d2be16426be4f1db1c74122eba516a8c45fc
oscar menéndez <omenendez7@gmail.com>
2020-12-14 23:27:13 -0600

    Remove unnecesary packages and add missing config

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import 'nextra-theme-docs/style.css'
+import 'nextra-theme-blog/style.css'
+
+import Head from 'next/head'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <link
+        rel="alternate"
+        type="application/rss+xml"
+        title="RSS"
+        href="/feed.xml"
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
